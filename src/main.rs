68893f90efa1618ea8a9f4a020ec3d353f2df438
Evie Viau <evie@uwueviee.live>
2021-07-27 09:33:22 -0400

    Add basic Actix example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, web, HttpResponse, get, App};
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    return HttpResponse::Ok().body("soon");
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/")
+                .service(index)
+        )
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
